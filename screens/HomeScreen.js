d10118789b26d5bd143ff11348157eaff5afdea3
Nicolas Corbalan <outreef@hotmail.com>
2022-07-23 22:11:15 +0200

    header

---
+++ screens/HomeScreen.js
@@ -0,0 +1,70 @@
+import { View, Text, SafeAreaView, TextInput, ScrollView } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+import {
+  UserIcon,
+  ChevronDownIcon,
+  SearchIcon,
+  AdjustmentsIcon,
+} from "react-native-heroicons/outline";
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
+  return (
+    <SafeAreaView>
+      {/* Header */}
+      <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
+        <Image
+          source={{
+            uri: "https://links.papareact.com/wru",
+          }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
+        </View>
+
+        <UserIcon size={35} color="#00CCBB" />
+      </View>
+
+      {/* Search */}
+      <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">
+        <View className="flex-row space-x-2 bg-gray-200 p-3">
+          <SearchIcon color="gray" size={20} />
+          <TextInput
+            placeholder="Restaurants and cuisines"
+            keyboardType="default"
+          />
+        </View>
+
+        <AdjustmentsIcon color="#00CCBB" />
+      </View>
+
+      {/* Body */}
+      <ScrollView
+        className="bg-gray-100"
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}
+      >
+        {/* Categories */}
+        <Categories />
+        {/* Featured Rows */}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
